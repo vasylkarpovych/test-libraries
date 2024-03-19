b366d88608d1563cccabdd8ae4d54b11a15887d9
Vasyl Karpovych <karpovichvasya@gmail.com>
2024-03-19 22:53:38 +0200

    test-libraries. First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// import { name } from "./module.js";
+// import "./style.css";
+
+// import isEmail from "validator/lib/isEmail";
+// import isURL from "validator/lib/isURL";
+// import toDate from "validator/lib/toDate";
+
+// // console.log("Your name is " + name);
+
+// console.log(isEmail("foo@bar.com")); //=> true
+// console.log(isURL("https://ukr-mova.in.ua/perevirka-tekstu"));
+// console.log(toDate("5.18.89"));
+
+// var moment = require("moment");
+
+// console.log(moment().format("MMMM Do YYYY, h:mm:ss a"));
+// console.log(moment().format("MMM Do YY"));
+// console.log(moment().format("dddd"));
